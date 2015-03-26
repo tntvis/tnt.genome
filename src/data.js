ba350e49ef947f96ae9847d7b8d396a43b90d101
Miguel Pignatelli <emepyc@gmail.com>
2015-03-26 14:15:13 +0000

    added ensembl data retrieval (taken from tnt.board)

--- src/data.js
+++ src/data.js
@@ -9,6 +9,47 @@ var data_gene = function () {
     var track = board.track.data();
     // .index("ID");
 
+    board.track.data.retriever.ensembl = function () {
+	var success = [function () {}];
+	var endpoint;
+	var eRest = ensemblRestAPI();
+	var update_track = function (obj) {
+	    var data_parent = this;
+	    // Object has loc and a plug-in defined callback
+	    var loc = obj.loc;
+	    var plugin_cbak = obj.on_success;
+	    eRest.call({
+		url: eRest.url[update_track.endpoint()](loc),
+		success: function (resp) {
+		    data_parent.elements(resp);
+
+		    // User defined
+		    for (var i=0; i<success.length; i++) {
+			success[i](resp);
+		    }
+
+		    // plug-in defined
+		    plugin_cbak();
+		}
+	    });
+	};
+	apijs (update_track)
+	    .getset ('endpoint');
+
+	// TODO: We don't have a way of resetting the success array
+	// TODO: Should this also be included in the sync retriever?
+	// Still not sure this is the best option to support more than one callback
+	update_track.success = function (cb) {
+	    if (!arguments.length) {
+		return success;
+	    }
+	    success.push (cb);
+	    return update_track;
+	};
+
+	return update_track;
+    };
+    
     var updater = board.track.data.retriever.ensembl()
         .endpoint("region")
     // TODO: If success is defined here, means that it can't be user-defined
