10d65afb6135f7215c167032ff95629282b243cb
Miguel Pignatelli <emepyc@gmail.com>
2015-11-30 16:15:13 +0000

    better spinner

--- src/spinner.js
+++ src/spinner.js
@@ -3,28 +3,32 @@ var spinner = function () {
     var elem;
     var sp = {};
 
-    sp.on = function (container, posY) {
+    sp.on = function (container, bgColor) {
         n++;
         if (n==1) {
             elem = container
                 .append("svg")
                 .attr("class", "tnt_spinner")
                 .attr("width", "30px")
-                .attr("height", ((posY*2)-30) + "px")
+                .attr("height", "30px")
                 .attr("xmls", "http://www.w3.org/2000/svg")
                 .attr("viewBox", "0 0 100 100")
                 .attr("preserveAspectRatio", "xMidYMid");
 
+
             elem
                 .append("rect")
-                .attr("x", '46.5')
-                .attr("y", '40')
+                .attr("x", '0')
+                .attr("y", '0')
                 .attr("width", "100")
                 .attr("height", "100")
-                .attr("fill", "none");
+                .attr("rx", '50')
+                .attr("ry", '50')
+                .attr("fill", bgColor);
+                //.attr("opacity", 0.6);
 
             for (var i=0; i<12; i++) {
-                tick(elem, i);
+                tick(elem, i, bgColor);
             }
 
         } else {
@@ -37,12 +41,12 @@ var spinner = function () {
     sp.off = function (container) {
         n--;
         if (!n) {
-            container.select(".tnt_spinner")
+            container.selectAll(".tnt_spinner")
                 .remove();
         }
     };
 
-    function tick (elem, i) {
+    function tick (elem, i, bgColor) {
         elem
             .append("rect")
             .attr("x", "46.5")
@@ -51,7 +55,7 @@ var spinner = function () {
             .attr("height", "20")
             .attr("rx", "5")
             .attr("ry", "5")
-            .attr("fill", "#666666")
+            .attr("fill", d3.rgb(bgColor).darker(2))
             .attr("transform", "rotate(" + (360/12)*i + " 50 50) translate(0 -30)")
             .append("animate")
             .attr("attributeName", "opacity")
